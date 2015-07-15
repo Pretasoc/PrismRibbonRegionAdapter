ec3a615172c42654eb24b04df59f172a1998c415
TW <tilli@weissenberger.at>
2015-07-15 21:40:54 +0200

    signed assemblies

--- Prism.RibbonRegionAdapter/Properties/AssemblyInfo.cs
+++ Prism.RibbonRegionAdapter/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Windows.Markup;
 [assembly: ComVisible(false)]
 [assembly: Guid("A0A36D3D-320F-4BBC-AD31-BFE3B20FBD4E")]
 
-[assembly: AssemblyVersion("0.20.0.0")]
+[assembly: AssemblyVersion("0.20.0.1")]
 [assembly: AssemblyInformationalVersion("0.20.*")]
 
 [assembly: XmlnsDefinition("http://www.codeplex.com/prism", "Prism.RibbonRegionAdapter")]
-[assembly: InternalsVisibleTo("Prism.RibbonRegionAdapter.Tests")]
+[assembly: InternalsVisibleTo("Prism.RibbonRegionAdapter.Tests,PublicKey=00240000048000009400000006020000002400005253413100040000010001003dc295e928a2b5e63bb90cccae3c7ad70b4585323bf4ed544a659adff053f5fdf808b6e76227b37b793f956686574550404ce0c6b54fa8b3603dcead2dd9d344aa6d782dc16a24fb23c658ee906488830764eb48422882040c7cd7bbb6fbeb0536d8aef9ec72601c7cb7293a4c57cce4f451dd9beeb4652a9e1d8d4244f6f4d8")]
