a0c3a062d64497991fda0746df19c35a0a014eb8
Philemon Eichin <phileicht@gmx.de>
2019-03-15 09:23:10 +0100

    Import existing code

---
+++ Prism.Fluent.RegionAdapter/RibbonRegionAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Regions;
+
+namespace Prism.Fluent.RegionAdapter
+{
+    public class RibbonRegionAdapter : RegionAdapterBase<>
+    {
+    }
+}
